195af81eee339561dbb2b244c92b607cae73d7d1
Benjamin Besse <benjamin.besse02@gmail.com>
2017-08-20 16:44:11 +0200

    add base64 functions

---
+++ src/components/infoBlock.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="info-block valign-wrapper">
+        <mu-icon :size="25" :value="icon" v-if="icon"></mu-icon>
+        <div class="message-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name    : 'infoBlock',
+        props   : {
+            icon : {
+                type : String,
+            },
+        },
+        data() {
+            return {
+
+            };
+        },
+    };
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+    @import "~styles/core";
+
+    .info-block {
+        padding: 10px;
+        border-left: 5px solid color('grey', 'base');
+        margin-bottom: 20px;
+
+        .mu-icon + .message-content {
+            padding-left: 10px;
+        }
+    }
+</style>
--- src/pages/modules/base64.vue
+++ src/pages/modules/base64.vue
@@ -1,15 +1,57 @@
 <template>
     <div class="page base64">
-        <h4>Base64</h4>
-
+        <info-block icon="info_outline">
+            Vous pouvez encoder la String que vous voulez en base64 ou bien décodé une String base64.
+        </info-block>
+        <mu-text-field
+            hintText="Texte ou base64"
+            multiLine
+            :rows="3"
+            :rowsMax="6"
+            fullWidth
+            v-model="value"
+        />
+        <div class="btn-bar">
+            <mu-flat-button label="Encode" v-on:click="encodeAction" secondary/>
+            <mu-flat-button label="Decode" v-on:click="decodeAction" secondary/>
+        </div>
+        <div class="result" v-if="result">
+            {{ result }}
+        </div>
     </div>
 </template>
 
 <script>
+    import infoBlock from '../../components/infoBlock.vue';
+
     export default {
         name : 'base64',
+        components : {
+            infoBlock,
+        },
+        data() {
+            return {
+                result : null,
+                value : null,
+            };
+        },
+        methods : {
+            encodeAction() {
+                this.result = btoa(unescape(encodeURIComponent(this.value)));
+            },
+            decodeAction() {
+                this.result = decodeURIComponent(escape(window.atob(this.value)));
+            },
+        },
     };
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
+    @import "~styles/core";
+
+    .base64 {
+        .btn-bar {
+            text-align: right;
+        }
+    }
 </style>
