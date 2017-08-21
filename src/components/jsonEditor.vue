f37607a55041073303c8c9d314fae2a5e862681c
Benjamin Besse <benjamin.besse02@gmail.com>
2017-08-21 22:11:07 +0200

    create jsonEditor component and parse json

---
+++ src/components/jsonEditor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div ref="jsoneditor" class="json-editor"></div>
+</template>
+
+<script>
+    import JsonEditor from 'jsoneditor';
+    import 'jsoneditor/dist/jsoneditor.css';
+
+    export default {
+        name  : 'jsonEditor',
+        props : {
+            value : Object,
+            options : {
+                type    : Object,
+                default() {
+                    return {};
+                },
+            },
+        },
+        data() {
+            return {
+                inputValue : this.value,
+            };
+        },
+        mounted() {
+            this.editor = new JsonEditor(this.$refs.jsoneditor, this.options);
+            this.editor.set(this.inputValue);
+        },
+    };
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+    @import "~styles/core";
+
+    .json-editor {
+        width: 100%;
+    }
+</style>
--- src/pages/modules/json.vue
+++ src/pages/modules/json.vue
@@ -1,10 +1,71 @@
-<template></template>
+<template>
+    <div class="page json">
+        <info-block icon="info_outline">
+            You can parse a jsonString and explore this easilly.
+        </info-block>
+        <mu-text-field
+            hintText="String to hash"
+            multiLine
+            :rows="3"
+            :rowsMax="6"
+            fullWidth
+            v-model="value"
+            :errorText="error"
+        />
+        <div class="btn-bar">
+            <mu-flat-button label="Parse" v-on:click="parseAction" secondary/>
+        </div>
+        <div class="result" v-if="result">
+            <h5>Result</h5>
+            <json-editor :options="options" :value="result"></json-editor>
+        </div>
+    </div>
+</template>
 
 <script>
+    import infoBlock from '../../components/infoBlock.vue';
+    import jsonEditor from '../../components/jsonEditor.vue';
+
     export default {
-        name : 'JsonParser',
+        name        : 'JsonParser',
+        components  : {
+            jsonEditor,
+            infoBlock,
+        },
+        data() {
+            return {
+                value   : null,
+                error   : null,
+                result  : null,
+                options : {
+                    mode   : 'view',
+                    search : true,
+                },
+            };
+        },
+        methods : {
+            parseAction() {
+                this.result = null;
+
+                if (!this.value) {
+                    this.error = 'This field is required';
+                    return;
+                }
+
+                try {
+                    this.result = JSON.parse(this.value);
+                } catch (e) {
+                    this.error = 'The JSON is invalid';
+                }
+            },
+        },
     };
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
+    .json {
+        .btn-bar {
+            text-align: right;
+        }
+    }
 </style>
