9aab3cbee540cfa65879bc6c8bd4eee8eabece45
Benjamin Besse <benjamin.besse02@gmail.com>
2017-10-05 13:16:08 +0200

    mise en forme de la page d'accueil

--- src/pages/modules/home.vue
+++ src/pages/modules/home.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="page home">
-        <h4>Home</h4>
+        <v-container grid-list-lg>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <v-card>
+                        <v-card-title>
+                            <div>
+                                <h3>Toolbox</h3>
+                                <div>This application aims to make life easier for developers by bringing together different tools in one place.</div>
+                                <br>
+                                <div>
+                                    For the moment it concentrates only on tools of voncersion. If you need more tools, or improve the existing ones, please make your proposal on the github Project
+                                </div>
+                            </div>
+                        </v-card-title>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
